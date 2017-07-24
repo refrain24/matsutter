30a77d954e7b51706ae1f6dcec4928dbe8fd88a3
Nanami Matsumoto <refrain_24@hotmail.co.jp>
2017-07-24 23:06:01 +0900

    commit

--- client/main.js
+++ client/main.js
@@ -6,53 +6,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session'
 
 import './main.html';
-import './tab.js';
 
+
+// This code only runs on the client
 if (Meteor.isClient) {
-   Meteor.subscribe('privateTweets');
+  var img = 'img/smile.png';
+  Meteor.subscribe('privateTweets');
 
-  // 'change #filter': function(e, template) {
-  //     var selected = e.target.options[e.target.selectedIndex].value;
-  //     // (1) セッションに選択値をセット
-  //     Session.set('filter', selected);
-  // },
-  // Meteor.autorun(function() {
-  //   Meteor.subscribe('privateTweets', Session.get('filter'));
-  // });
-  // テンプレート内のemployeesという変数の値を返す
-  // Template.employeeList.tweets = function() {
-  //     // コレクション内の全データを返す
-  //     var cursor = Tweets.find();
-  //     // カーソルの状態を監視してログを残す
-  //     cursor.observe({
-  //         // 検索結果が増やされた
-  //         added: function(document, beforeIndex) {
-  //             console.log('added(追加された位置:' + beforeIndex + ')');
-  //         },
-  //         // 検索結果が変更された
-  //         changed: function(newDocument, atIndex, oldDocument) {
-  //             console.log('changed(位置:' + atIndex + ')');
-  //         },
-  //         // 検索結果内でオブジェクトのインデックスが変わった
-  //         moved: function(document, oldIndex, newIndex) {
-  //             console.log('moved(前の位置:' + oldIndex + ' 後の位置:' + newIndex + ')');
-  //         },
-  //         // 検索結果からオブジェクトが減った
-  //         removed: function(oldDocument, atIndex) {
-  //             console.log('removed(位置:' + atIndex + ')');
-  //         }
-  //     });
-  //     return cursor;
-  // };
-
-//   Template.tweet.events = {
-//       'change #filter': function(e, template) {
-//           var selected = e.target.options[e.target.selectedIndex].value;
-//           Session.set('filter', selected);
-//       },
-// }
-
-  // This code only runs on the client
   Template.body.helpers({
     tweets: function() {
       return Tweets.find({}, {
@@ -62,20 +22,19 @@ if (Meteor.isClient) {
       });
     }
   })
-  // Template.tweet.helpers({
-  //   'tweet': function () {
-  //       var uid = Meteor.userId();
-  //       return ChatComments.find()
-  //   }
-  // });
   Template.body.events({
-    'submit form': function (event) {
-        event.preventDefault();
-        var text = event.target.text.value;
-        var username = Meteor.user().username;
-        Meteor.call('tweetList',text);
-        // Clear form
-        event.target.text.value = "";
+    'submit form': function(event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      var username = Meteor.user().username;
+      var img = 'img/smile.png';
+      Meteor.call('tweetList', text);
+      // Clear form
+      event.target.text.value = "";
+      var uid = this.userId;
+      console.log(Tweets.find({
+        userId: uid
+      }));
     }
   });
   //UserNameでログインできるようにする
--- server/main.js
+++ server/main.js
@@ -3,57 +3,36 @@ import { HTTP } from 'meteor/http'
 
 Meteor.startup(() => {
   Tweets = new Mongo.Collection("tweets");
-  // code to run on server at startup
 });
+// code to run on server at startup
 if (Meteor.isServer) {
-        Meteor.publish('privateTweets',function(){
-                var uid=this.userId;
-                return Tweets.find({})
-        });
-        // Meteor.publish('publicTweets',function(){
-        //         var uid=this.userId;
-        //         return Tweets.find({})
-        // });
-        Meteor.methods({
-          'tweetList': function(text) {
-            var uid = this.userId;
+  Meteor.publish('privateTweets', function() {
+    var uid = this.userId;
+    return Tweets.find({})
+  });
+  Meteor.methods({
+    'tweetList': function(text) {
+      var uid = this.userId;
 
-            // 感情分析api呼び出し
-            var apiKey = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + process.env.API_KEY;
-            var result = HTTP.call('POST', apiKey, {
-              data: {
-                "encodingType": "UTF8",
-                "document": {
-                  "type": "PLAIN_TEXT",
-                  "content": text
-                }
-              }
-            }, function(error, result) {
-              Tweets.insert({
-                text: text,
-                userId: Meteor.userId(),
-                username: Meteor.user().username,
-                createdAt: new Date(),
-                score: result.data.documentSentiment.score
-              });
-            });
+      // 感情分析api呼び出し
+      var url = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + process.env.API_KEY;
+      var result = HTTP.call('POST', url, {
+        data: {
+          "encodingType": "UTF8",
+          "document": {
+            "type": "PLAIN_TEXT",
+            "content": text
           }
+        }
+      }, function(error, result) {
+        Tweets.insert({
+          text: text,
+          userId: Meteor.userId(),
+          username: Meteor.user().username,
+          createdAt: new Date(),
+          score: result.data.documentSentiment.score
         });
-
-        // Meteor.publish("privateTweets", function (filter) {
-        //     // クライアントから送信された検索条件に応じて，処理が変わる
-        //     // 検索条件の指定なし
-        //     if (!filter) {
-        //         return Tweets.find(); // everything
-        //     }
-        //     var minAge = parseInt(filter);
-        //     var uid=this.userId;
-        //     switch (minAge) {
-        //     // 60歳以上
-        //     case 60:
-        //         return Tweets.find({userId:uid});
-        //     default:
-        //         return Tweets.find({userId:uid});
-        //     }
-        // });
+      });
+    }
+  });
 }
