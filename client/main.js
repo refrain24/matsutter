4dc807d91a675222e1368440dd6200b3720b5be6
Nanami Matsumoto <refrain_24@hotmail.co.jp>
2017-07-24 12:06:57 +0900

    commit

--- client/main.js
+++ client/main.js
@@ -1,43 +1,86 @@
-Tasks = new Mongo.Collection("tasks");
+  (function() {
+  Tweets = new Mongo.Collection("tweets");
 
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session'
 
 import './main.html';
 import './tab.js';
 
 if (Meteor.isClient) {
-  Meteor.subscribe('privateTweets');
+   Meteor.subscribe('privateTweets');
+
+  // 'change #filter': function(e, template) {
+  //     var selected = e.target.options[e.target.selectedIndex].value;
+  //     // (1) セッションに選択値をセット
+  //     Session.set('filter', selected);
+  // },
+  // Meteor.autorun(function() {
+  //   Meteor.subscribe('privateTweets', Session.get('filter'));
+  // });
+  // テンプレート内のemployeesという変数の値を返す
+  // Template.employeeList.tweets = function() {
+  //     // コレクション内の全データを返す
+  //     var cursor = Tweets.find();
+  //     // カーソルの状態を監視してログを残す
+  //     cursor.observe({
+  //         // 検索結果が増やされた
+  //         added: function(document, beforeIndex) {
+  //             console.log('added(追加された位置:' + beforeIndex + ')');
+  //         },
+  //         // 検索結果が変更された
+  //         changed: function(newDocument, atIndex, oldDocument) {
+  //             console.log('changed(位置:' + atIndex + ')');
+  //         },
+  //         // 検索結果内でオブジェクトのインデックスが変わった
+  //         moved: function(document, oldIndex, newIndex) {
+  //             console.log('moved(前の位置:' + oldIndex + ' 後の位置:' + newIndex + ')');
+  //         },
+  //         // 検索結果からオブジェクトが減った
+  //         removed: function(oldDocument, atIndex) {
+  //             console.log('removed(位置:' + atIndex + ')');
+  //         }
+  //     });
+  //     return cursor;
+  // };
+
+//   Template.tweet.events = {
+//       'change #filter': function(e, template) {
+//           var selected = e.target.options[e.target.selectedIndex].value;
+//           Session.set('filter', selected);
+//       },
+// }
 
   // This code only runs on the client
   Template.body.helpers({
-  tasks: function () {
-    return Tasks.find({}, {sort: {createdAt: -1}});
-  }
-})
-Template.body.events({
-  "submit form": function (event) {
-    event.preventDefault();
-    var text = event.target.text.value;
-    var dateObj = new Date() ;
-    // 日時の各情報を取得
-    var year = dateObj.getFullYear() ;	// 年
-    var month = dateObj.getMonth() + 1 ;	// 月
-    var date = dateObj.getDate() ;	// 日
-    var hour = dateObj.getHours() ;	// 時
-    var minute = dateObj.getMinutes() ;	// 分
-    // 表示用に組み立てる ( → 2016年7月2日 15時57分 )
-    var date = year + "年" + month + "月" + date + "日  " + hour + "時" + minute + "分" ;
-    Tasks.insert({
-      text: text,
-      createdAt: dateObj, // current time
-      userId: Meteor.userId(),
-      username: Meteor.user().username
-    });
-    event.target.text.value = "";
-  }
-})
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+    tweets: function() {
+      return Tweets.find({}, {
+        sort: {
+          createdAt: -1
+        }
+      });
+    }
+  })
+  // Template.tweet.helpers({
+  //   'tweet': function () {
+  //       var uid = Meteor.userId();
+  //       return ChatComments.find()
+  //   }
+  // });
+  Template.body.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var text = event.target.text.value;
+        var username = Meteor.user().username;
+        Meteor.call('tweetList',text);
+        // Clear form
+        event.target.text.value = "";
+    }
+  });
+  //UserNameでログインできるようにする
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
+})();
--- server/main.js
+++ server/main.js
@@ -1,16 +1,57 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http'
 
 Meteor.startup(() => {
-  Tasks = new Mongo.Collection("tasks");
+  Tweets = new Mongo.Collection("tweets");
   // code to run on server at startup
 });
 if (Meteor.isServer) {
         Meteor.publish('privateTweets',function(){
                 var uid=this.userId;
-                return Tasks.find({userId:uid})
+                return Tweets.find({})
         });
-        Meteor.publish('publicTweets',function(){
-                var uid=this.userId;
-                return Tasks.find({})
+        // Meteor.publish('publicTweets',function(){
+        //         var uid=this.userId;
+        //         return Tweets.find({})
+        // });
+        Meteor.methods({
+          'tweetList': function(text) {
+            var uid = this.userId;
+
+            // 感情分析api呼び出し
+            var result = HTTP.call('POST', 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=env.API_KEY', {
+              data: {
+                "encodingType": "UTF8",
+                "document": {
+                  "type": "PLAIN_TEXT",
+                  "content": text
+                }
+              }
+            }, function(error, result) {
+              Tweets.insert({
+                text: text,
+                userId: Meteor.userId(),
+                username: Meteor.user().username,
+                score: result.data.documentSentiment.score
+              });
+            });
+          }
         });
+
+        // Meteor.publish("privateTweets", function (filter) {
+        //     // クライアントから送信された検索条件に応じて，処理が変わる
+        //     // 検索条件の指定なし
+        //     if (!filter) {
+        //         return Tweets.find(); // everything
+        //     }
+        //     var minAge = parseInt(filter);
+        //     var uid=this.userId;
+        //     switch (minAge) {
+        //     // 60歳以上
+        //     case 60:
+        //         return Tweets.find({userId:uid});
+        //     default:
+        //         return Tweets.find({userId:uid});
+        //     }
+        // });
 }
